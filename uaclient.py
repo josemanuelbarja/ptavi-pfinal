59ad1d75e794559db18bc8f38f4adf227caf3c46
José Manuel Barja Vázquez <josemanuelbarja@MacBook-Pro-de-Jose-5.local>
2019-01-08 20:49:20 +0100

    Terminada clase LOGGER y definido objeto log

--- uaclient.py
+++ uaclient.py
@@ -8,6 +8,7 @@ import socket
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 atts = {'account': ['username', 'passwd'],
         'uaserver': ['ip', 'puerto'],
         'rtpaudio': ['puerto'],
@@ -33,45 +34,104 @@ class Default(dict):
      def __missing__(self, key):
          return key
 
+class Logger:
+
+    def __init__(self,fich):
+        if not os.path.exists(fich):
+            self.log = open(fich, 'w')
+        self.fich = fich
+        self.log = open(self.fich, 'a')
+        self.log.write(time.strftime('%Y%m%d%H%M%S') + ' Starting...\r\n')
+        self.log.close()
+
+    def send(self,address,message):
+        self.log = open(self.fich, 'a')
+        self.log.write((time.strftime('%Y%m%d%H%M%S') + ' Sent to ' + address +
+            ': ' + message))
+        self.log.close()
+
+    def received(self,address,message):
+        self.log = self.log = open(self.fich, 'a')
+        self.log.write((time.strftime('%Y%m%d%H%M%S') + ' Received from ' +
+            address + ': ' + message))
+        self.log.close()
+
+    def error(self, message):
+        self.log = self.log = open(self.fich, 'a')
+        self.log.write((time.strftime('%Y%m%d%H%M%S') + ' Error: '
+            + message))
+        self.log.close()
+
+    def finishing(self):
+        self.log = open(self.fich, 'a')
+        self.log.write(time.strftime('%Y%m%d%H%M%S') + ' Finishing.\r\n')
+        self.log.close()
+
 class SendSip:
 
     def __init__(self, server, port):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as self.my_socket:
-            self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.my_socket.connect((server, port))
-            if METHOD == 'REGISTER':
-                self.processRegister()
-            elif METHOD == 'INVITE':
-                self.processInvite()
-            elif METHOD == 'BYE':
-                self.processBye()
-            data = self.my_socket.recv(1024)
-            self.response = data.decode('utf-8').split("\r\n")
-            self.processResponse()
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as self.sock:
+                self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                self.sock.connect((server, port))
+                if METHOD == 'REGISTER':
+                    self.processRegister()
+                elif METHOD == 'INVITE':
+                    self.processInvite()
+                elif METHOD == 'BYE':
+                    self.processBye()
+                try:
+                    data = self.sock.recv(1024)
+                    self.response = data.decode('utf-8').split("\r\n")
+                    print(' '.join(self.response))
+                    log.received(praddress, ' '.join(self.response) + '\r\n')
+                    self.processResponse()
+                except:
+                    message = ('No server listening at ' + proxyip + ' port '
+                        + str(proxyport))
+                    log.error(message + '\r\n')
+                    log.finishing()
+                    sys.exit(message)
+
 
     def processRegister(self):
-        self.my_socket.send((bytes(MESSAGE.format_map(Default(name= user))
-            + EXPIRES,'utf-8') + b'\r\n'))
+        self.sock.send((bytes(MESSAGE.format_map(Default(name= user))
+            + '\r\n' + EXPIRES + '\r\n','utf-8')))
+        log.send(praddress, MESSAGE.format_map(Default(name= user))+ EXPIRES +
+        '\r\n')
 
     def processInvite(self):
-        self.my_socket.send((bytes(MESSAGE.format_map(Default(name= OPTIONS))
+        self.sock.send((bytes(MESSAGE.format_map(Default(name= OPTIONS))
             ,'utf-8')))
-        self.my_socket.send(b'Content-type: application/sdp\r\n')
-        self.my_socket.send(bytes('v=0\r\n' + 'o=' + usrname + " " + serverip
-            + '\r\n' + 's=sipsesion\r\n' + 't=0\r\n' + 'm=audio ' + str(rtpport)
-            + ' RTP\r\n','utf-8'))
+        headersdp = ('Content-type: application/sdp\r\n\r\n' + 'v=0\r\n' + 'o=' +
+            usrname + " " + serverip + '\r\n' + 's=sipsesion\r\n' + 't=0\r\n' +
+             'm=audio ' + str(rtpport) + ' RTP\r\n')
+        self.sock.send(bytes(headersdp,'utf-8'))
+        log.send(praddress, MESSAGE.format_map(Default(name= OPTIONS)))
 
     def processBye(self):
-        self.my_socket.send((bytes(MESSAGE.format_map(Default(name= OPTIONS))
-            ,'utf-8')))
+        self.sock.send((bytes(MESSAGE.format_map(Default(name= OPTIONS))
+            + '\r\n','utf-8')))
 
     def processResponse(self):
         if '401' in self.response[0]:
-            print(self.response)
+            #print(self.response)
             nonce = self.response[2].split("nonce=")[1]
-            self.my_socket.send((bytes(MESSAGE.format_map(Default(name= user))
-                + EXPIRES + '\r\n' + 'Authorization: Digest response=' + nonce
-                + '\r\n','utf-8')))
+            authen = 'Authorization: Digest response=' + nonce + '\r\n'
+            self.sock.send((bytes(MESSAGE.format_map(Default(name= user))
+                + '\r\n' + EXPIRES + '\r\n' + authen,'utf-8')))
+            log.send(praddress, MESSAGE.format_map(Default(name= user)) +
+            EXPIRES + ' ' + authen)
+        try:
+            data = self.sock.recv(1024)
+            self.response = data.decode('utf-8').split("\r\n")
+            print(' '.join(self.response))
+            log.received(praddress, ' '.join(self.response) + '\r\n')
+        except:
+            message = ('No server listening at ' + proxyip + ' port '
+                + str(proxyport))
+            log.error(message + '\r\n')
+            log.finishing()
+            sys.exit(message)
 
 if __name__ == '__main__':
     try:
@@ -93,8 +153,11 @@ if __name__ == '__main__':
     rtpport = int(values['rtpaudio:puerto'])
     proxyip = values['regproxy:ip']
     proxyport = int(values['regproxy:puerto'])
+    praddress = proxyip + ':' + str(proxyport)
     fichlog = values['log:path']
     audio = values['audio:path']
-    MESSAGE = METHOD + ' sip: {name}' + ' SIP/2.0\r\n'
-    EXPIRES = 'EXPIRES: ' + OPTIONS #  'Only used with REGISTER method'
-    connect = SendSip(serverip, proxyport)
+    MESSAGE = METHOD + ' sip: {name}' + ' SIP/2.0 '
+    EXPIRES = 'EXPIRES: ' + OPTIONS  #  'Only used with REGISTER method'
+    log = Logger(fichlog)
+    connect = SendSip(proxyip, proxyport)
+    log.finishing()
